2597700126a5bf2d46b7106b82f822328db95987
Danial.A <32658241+DanialAroff@users.noreply.github.com>
2019-10-03 17:44:33 +0800

    revised Question class

--- QuizGameRe/src/quizgamere/QuizGame.java
+++ QuizGameRe/src/quizgamere/QuizGame.java
@@ -1,14 +1,15 @@
 
 package quizgamere;
 
-import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.Collections;
 
 class Question {
     String question;
-    HashMap<String, String> answers; //<key (A,B,C,D), values (answer)> 
+    ArrayList<String> answers; 
     String correctAns; // A, B, C or D
     
-    Question (String question, HashMap answers, String correctAns) {
+    Question (String question, ArrayList answers, String correctAns) {
         this.question = question;
         this.answers = answers;
         this.correctAns = correctAns;
@@ -17,7 +18,7 @@ class Question {
     public String getQuestion() {
         return question;
     }
-    public HashMap getAnswers() {
+    public ArrayList getAnswers() {
         return answers;
     }
     
@@ -28,6 +29,10 @@ class Question {
      */
     public boolean isCorrect(String ans) {
         return ans.equals(correctAns);
+    }
+    
+    public void shuffleAnswers() {
+        Collections.shuffle(answers);
     }
 } 
 
--- QuizGameRe/src/quizgamere/QuizGameRe.java
+++ QuizGameRe/src/quizgamere/QuizGameRe.java
@@ -1,10 +1,26 @@
 
 package quizgamere;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class QuizGameRe {
 
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner s;
+        try {
+            s = new Scanner(new File("game_questions.csv"), "UTF-8");
+            s.useDelimiter(",");
+            while(s.hasNext()) {
+                System.out.print(s.next() + " ");
+            }
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(QuizGameRe.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
     }
     
 }
